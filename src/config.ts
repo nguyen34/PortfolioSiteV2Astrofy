dba26ae165fb8d81694eb7e5f91796a550393ba0
nguyen34 <nguyenjohnson65@gmail.com>
2023-07-09 22:17:01 -0700

    Updated metadata

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Johnson Nguyen';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'Personal Portfolio website for Johnson Nguyen, Full Stack Software Engineer. Built with Astro and Tailwind CSS';
